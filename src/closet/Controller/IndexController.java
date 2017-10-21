c4d5d238d49d1e703ebebe7b481adf0447df90d2
Katya Nicoara <katya.nicoara@gmail.com>
2017-10-21 13:33:46 -0400

    setting project structure

---
+++ src/closet/Controller/IndexController.java
@@ -0,0 +1,13 @@
+package closet.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(Model m) {
+        m.addAttribute("someAttribute", "someValue");
+        return "index";
+    }
+}
--- web/WEB-INF/views/index.jsp
+++ web/WEB-INF/views/index.jsp
@@ -11,6 +11,8 @@
     <title>$Title$</title>
   </head>
   <body>
+
+  <p>${someAttribute}</p>
   $END$
   </body>
 </html>
