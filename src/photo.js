90ff845ed0d4450c02338dde2a33fb534d7c91f6
Dani Banbahji <dbanbahji@gmail.com>
2017-11-08 19:44:40 -0500

    auth state change-please user-not auth.current user

--- src/photo.js
+++ src/photo.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
 import firebase from 'firebase';
 import FileUploader from 'react-firebase-file-uploader';
-import './App.css';
 
+class Photo extends Component {
+  state = {
+    username: '',
+    avatar: '',
+    isUploading: false,
+    progress: 0,
+    avatarURL: ''
+  };
 
-
-
-class Photo extends React.Component {
-  constructor(props){
-    super(props);
+  handleChangeUsername = (event) => this.setState({username: event.target.value});
+  handleUploadStart = () => this.setState({isUploading: true, progress: 0});
+  handleProgress = (progress) => this.setState({progress});
+  handleUploadError = (error) => {
+    this.setState({isUploading: false});
+    console.error(error);
   }
+  handleUploadSuccess = (filename) => {
+    this.setState({avatar: filename, progress: 100, isUploading: false});
+    firebase.storage().ref('images').child(filename).getDownloadURL().then(url => this.setState({avatarURL: url}));
+  };
 
-
-  render(){
-  		return (
-  			<div className="photo">
-   				<progress value="0" max="100" id="uploader">0%</progress>
-				<input type='file' value='upload' id='filebutton' />
-	  		</div>
-  	);
+  render() {
+  	firebase.auth().onAuthStateChanged(function(user) {
+  		if (user) {
+   			console.log('i hate you');
+  		} else {
+    // No user is signed in.
   }
- }
+});
+  	
+    return (
+      <div>
+        <form>
+          <label>Username:</label>
+          <input type="text" value={this.state.username} name="username" onChange={this.handleChangeUsername} />
+          <label>Avatar:</label>
+          {this.state.isUploading &&
+            <p>Progress: {this.state.progress}</p>
+          }
+          {this.state.avatarURL &&
+            <img src={this.state.avatarURL} />
+          }
+          <FileUploader
+            accept="image/*"
+            name="avatar"
+            randomizeFilename
+            storageRef={firebase.storage().ref('images')}
+            onUploadStart={this.handleUploadStart}
+            onUploadError={this.handleUploadError}
+            onUploadSuccess={this.handleUploadSuccess}
+            onProgress={this.handleProgress}
+          />
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Photo;
