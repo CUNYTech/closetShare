966633d6e950af94849e0f17f4377d1e87f436ef
Dani Banbahji <dbanbahji@gmail.com>
2017-11-08 20:01:02 -0500

    added footer router

--- src/entry.js
+++ src/entry.js
@@ -3,15 +3,20 @@ import { Route, Redirect, Switch, Link, HashRouter} from 'react-router-dom';
 import Play from './Play';
 import Home from './home';
 import Checkout from './checkout';
-import Photo from './photo'
+import Photo from './photo';
+import Footer from './footer';
+
 
 const Entry = (props) => {
     return (
         <div>
-            <Route exact path="/" component={ Play } />
-            <Route exact path="/home" component={ Home } />
-            <Route exact path="/checkout" component={Checkout} />
-            <Route exact path="/photos" component={Photo} />
+        	<Switch>
+            	<Route exact path="/" component={ Play } />
+            	<Route exact path="/home" component={ Home } />
+            	<Route exact path="/checkout" component={Checkout} />
+            	<Route exact path="/photos" component={Photo} />
+            </Switch>
+            <Footer />
         </div>
   );
 };
---
+++ src/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Entry from './entry';
+import './App.css';
+import { Route, Redirect, Switch, Link, HashRouter} from 'react-router-dom';
+
+
+const Footer = () => {
+  return (
+    <div className='footer'>Hello</div>
+  );
+};
+
+
+export default Footer;
