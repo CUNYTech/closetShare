69d760e254edf2cca0b3e614d2b87d0e21392a5c
bmesa620 <bmesa620@gmail.com>
2017-11-13 21:28:22 -0500

    product listing

--- src/productedit.js
+++ src/productedit.js
@@ -4,6 +4,12 @@ import firebase, { auth, provider } from './firebase.js';
 import { Route, Redirect, Switch, Link, HashRouter} from 'react-router-dom';
 
 class Product extends Component {
+    //need
+    // item view
+    // edit or delete upon
+    // edit will open current info and allow update
+    // delete will destroy item reroute back to products
+    // only user created items
 
 };
 
