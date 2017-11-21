9906b373f285e9ea54a48235603d82c50e30d944
Katya Nicoara <katya.nicoara@gmail.com>
2017-11-20 19:30:56 -0500

    registration and login page

--- src/entry.js
+++ src/entry.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Route, Redirect, Switch, Link, HashRouter} from 'react-router-dom';
-import Play from './Play';
 import Home from './home';
 import Checkout from './checkout';
+import Register from './register';
+import Signin from './signin';
+import Profile from './profile';
+
 
 
 const Entry = (props) => {
     return (
         <div>
-            <Route exact path="/" component={ Play } />
-            <Route exact path="/home" component={ Home } />
+            <Route exact path="/" component={ Home } />
             <Route exact path="/checkout" component={Checkout} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/signin" component={Signin} />
+            <Route exact path="/profile" component={Profile} />
         </div>
   );
 };
--- src/home.js
+++ src/home.js
@@ -6,8 +6,14 @@ import { Route, Redirect, Switch, Link, HashRouter} from 'react-router-dom';
 const Home = () => {
   return (
     <div className='home'>
-   adgdzhdfhdfhdfghdf
-   <Link to="/checkout" style={{color: 'black'}} activeStyle={{color: 'red'}}>Checkout</Link>
+  
+
+   <Link to="/checkout" style={{color: 'black'}} >Checkout</Link>
+   <br />
+   <Link to="/register" style={{color: 'black'}} >Register</Link>
+   <br />
+   <Link to="/signin" style={{color: 'black'}} >Sign in</Link>
+   
   </div>
   );
 };
