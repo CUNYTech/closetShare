19045c9026e6d171be43abc5734ed81c267ed494
sayaka-tamura <arccycle@gmail.com>
2017-11-29 20:15:11 -0500

    Changed around material-ui

--- src/checkout.js
+++ src/checkout.js
@@ -1,13 +1,12 @@
 import React from 'react';
-
+import RaisedButton from 'material-ui/RaisedButton';
 
 const Checkout = () => {
   return (
-    <div className='checkout'>
-    We are at Checkout!!
-  </div>
+      <div className='checkout'>
+      We are at Checkout!!
+      </div>
   );
 };
-
 
 export default Checkout;
--- src/root.js
+++ src/root.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Entry from './entry';
 import { Route, Redirect, Switch, Link, HashRouter} from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 const Root = ({ }) => (
-  
+  <MuiThemeProvider>
     <HashRouter>
       <Entry />
     </HashRouter>
+  </MuiThemeProvider>
 );
 
 export default Root;
