4346b4366dc23aacac6c10d338e8b04f2c6e9989
Dani Banbahji <dbanbahji@gmail.com>
2017-12-05 21:09:28 -0500

    storage plus write

--- src/photo.js
+++ src/photo.js
@@ -1,11 +1,62 @@
 import React, { Component } from 'react';
-import firebase from 'firebase';
+import firebase, { auth, provider } from './firebase.js';
+import './photo.css';
 
 
 class Photo extends Component {
 
-  constructor(props){
-    super(props);
+  constructor(){
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user });
+      }
+    });
+  }
+
+  handleChange(event) {
+    var uploader = document.getElementById('uploader');
+    //get file from dom
+    var file = event.target.files[0];
+    // console.log(file);
+    //create storage ref
+    var storageRef = firebase.storage().ref('images/' + file.name);
+    var task = storageRef.put(file);
+    task.on('state_changed',
+      function progress(snapshot){
+        var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        uploader.value = percentage;
+      },
+
+      function error(err){
+        console.log(err);
+      },
+
+      function complete(){
+        //remember to invoke
+        var downloadURL = storageRef.getDownloadURL().then(function(url) {
+          //test to see if url works
+          document.getElementById('url').innerHTML = url ;
+          console.log(url);
+          //push to firebase items write to database
+          auth.onAuthStateChanged((user) => {
+            if (user) {
+              const itemsRef = firebase.database().ref('items');
+              const item = {
+                image: user.displayName,
+                user: user.email
+              };
+                itemsRef.push(item);
+              }
+          });
+        });
+      }
+    );
   }
 
 
@@ -13,7 +64,13 @@ class Photo extends Component {
 
 
     return (
-       <input type="file" id="fileButton"/>
+      <div>
+          <progress value="0" max="100" id="uploader">0%</progress>
+          <input type='file'  id='fileButton' onChange={this.handleChange}  />
+          <div id="notice_wrapper">
+            <p id='url'></p>
+          </div>
+       </div>
     );
   }
 }
