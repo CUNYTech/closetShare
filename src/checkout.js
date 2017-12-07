a1d24a18ba74442a201d6f0c580fedd1acf3d1b8
Dani Banbahji <dbanbahji@gmail.com>
2017-12-06 19:39:28 -0500

    resolve conflict

--- src/checkout.js
+++ src/checkout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import firebase from 'firebase';
+import RaisedButton from 'material-ui/RaisedButton';
 
 
 const Checkout = () => {
--- src/entry.js
+++ src/entry.js
@@ -5,16 +5,27 @@ import Home from './home';
 import Checkout from './checkout';
 import Photo from './photo';
 import Footer from './footer';
+import Register from './register';
+import Signin from './signin';
+import Profile from './profile_pages/profile';
+import Product from './Product_Pages/product';
+import ProductCreate from './Product_Pages/productcreate';
+import ProductDetail from './Product_Pages/productdetail';
 
 
 const Entry = (props) => {
     return (
         <div>
         	<Switch>
-            	<Route exact path="/" component={ Play } />
-            	<Route exact path="/home" component={ Home } />
-            	<Route exact path="/checkout" component={Checkout} />
-              <Route exact path="/photos" component={Photo} />
+            <Route exact path="/" component={ Home } />
+            <Route exact path="/checkout" component={Checkout} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/signin" component={Signin} />
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/product" component={Product} />
+            <Route exact path="/product/create" component={ProductCreate} />
+            <Route exact path="/product/productdetail/:id" component={ProductDetail} />
+            <Route exact path="/photos" component={Photo} />
             </Switch>
         <Footer />
         </div>
