617872ed448eafb0353e7820219a5c031f820861
Dani Banbahji <dbanbahji@gmail.com>
2017-12-06 19:51:03 -0500

    merge errors

--- src/checkout.js
+++ src/checkout.js
@@ -1,14 +1,9 @@
 import React from 'react';
-<<<<<<< HEAD
 import firebase from 'firebase';
 import RaisedButton from 'material-ui/RaisedButton';
 
-======
->>>>>>> master
-
 const Checkout = () => {
   let images =  firebase.storage().ref('images');
-  debugger;
   return (
       <div className='checkout'>
       We are at Checkout!!
--- src/entry.js
+++ src/entry.js
@@ -2,26 +2,19 @@ import React from 'react';
 import { Route, Redirect, Switch, Link, HashRouter} from 'react-router-dom';
 import Home from './home';
 import Checkout from './checkout';
-<<<<<<< HEAD
-=======
->>>>>>> master
+import Photo from './Photos'
 import Register from './register';
 import Signin from './signin';
 import Profile from './profile_pages/profile';
 import Product from './Product_Pages/product';
 import ProductCreate from './Product_Pages/productcreate';
 import ProductDetail from './Product_Pages/productdetail';
-<<<<<<< HEAD
-
-=======
->>>>>>> master
 
 const Entry = (props) => {
     return (
         <div>
-<<<<<<< HEAD
         	<Switch>
-=======
+
 >>>>>>> master
             <Route exact path="/" component={ Home } />
             <Route exact path="/checkout" component={Checkout} />
@@ -29,15 +22,13 @@ const Entry = (props) => {
             <Route exact path="/signin" component={Signin} />
             <Route exact path="/profile" component={Profile} />
             <Route exact path="/product" component={Product} />
-<<<<<<< HEAD
             <Route exact path="/product/create" component={ProductCreate} />
             <Route exact path="/product/productdetail/:id" component={ProductDetail} />
             <Route exact path="/photos" component={Photo} />
             </Switch>
         <Footer />
 =======
-            <Route exact path="/product/create/:id" component={ProductCreate} />
-            <Route exact path="/product/productdetail/:id" component={ProductDetail} />
+
 >>>>>>> master
         </div>
   );
